e3ed4e9df23225543a91fbeba89e96c48d0c20bf
sujit <sujitbokade2014@gmail.com>
2022-09-06 19:27:34 +0530

    [Sujit]Area & Perimeter of Circle

--- src/com/bridgelabz/DemoProblems.java
+++ src/com/bridgelabz/DemoProblems.java
@@ -5,20 +5,12 @@ import java.util.Scanner;
 public class DemoProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("***Area of Circle***");
-
         System.out.println("Enter Radius of Circle(in cm)");
         double radius = sc.nextDouble();
         double area = 3.14*radius*radius;
+        double perimeter = 2 * 3.14 * radius;
 
         System.out.println("Area of Circle: " + area + " Sq.cm");
-
-        System.out.println("***Perimeter of Circle***");
-        System.out.println("Enter Radius of Circle(in cm)");
-        double circleRadius = sc.nextDouble();
-
-        double perimeter = 2 * 3.14 * circleRadius;
-
         System.out.println("Perimeter of Circle: " + perimeter + " cm");
 
 
