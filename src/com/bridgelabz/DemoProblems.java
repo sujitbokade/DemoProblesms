c398be5bc4a62bcff01bb2415a4ee997d380444a
sujit <sujitbokade2014@gmail.com>
2022-09-06 12:26:19 +0530

    [Sujit]Area and Perimeter of Triangle

--- src/com/bridgelabz/DemoProblems.java
+++ src/com/bridgelabz/DemoProblems.java
@@ -5,27 +5,29 @@ import java.util.Scanner;
 public class DemoProblems {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("***Area of Rectangle***");
+        System.out.println("***Area of Triangle***");
 
-        System.out.println("Enter First Side(in cm)");
-        double firstSide = sc.nextDouble();
-        System.out.println("Enter Second Side(in cm)");
-        double secondSide = sc.nextDouble();
-        double area = firstSide * secondSide;
+        System.out.println("Enter Base of Triangle(in cm)");
+        double base = sc.nextDouble();
+        System.out.println("Enter Perpendicular Height of Triangle(in cm)");
+        double height = sc.nextDouble();
+        double area = (base * height) / 2;
 
-        System.out.println("Area of Rectangle: " + area + " Sq.cm");
+        System.out.println("Area of Triangle: " + area + " Sq.cm");
 
-        System.out.println("***Perimeter of Rectangle***");
+        System.out.println("***Perimeter of Triangle***");
         System.out.println("Enter First Side(in cm)");
         double fSide = sc.nextDouble();
         System.out.println("Enter Second Side(in cm)");
         double sSide = sc.nextDouble();
-        double perimeter = 2*(fSide + sSide);
+        System.out.println("Enter Third Side(in cm)");
+        double tSide = sc.nextDouble();
+        double perimeter = (fSide + sSide + tSide);
 
-        System.out.println("Perimeter of Rectangle: " + perimeter + " cm");
+        System.out.println("Perimeter of Triangle: " + perimeter + " cm");
 
 
     }
 
- }
+}
 
